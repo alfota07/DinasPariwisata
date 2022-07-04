8049049ed2e5b80bc7b9298bfc091723adebd666
kurok <alfinsasga698@gmail.com>
2022-07-04 11:11:07 +0700

    期末楼

---
+++ app/src/main/java/com/example/dinaspariwisata/HomeMasuk.java
@@ -0,0 +1,14 @@
+package com.example.dinaspariwisata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeMasuk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_masuk);
+    }
+}
